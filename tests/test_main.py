9327279fdfbb6ee9f458e04997b1f3f14566f52a
Remco <dewilderemco13@gmail.com>
2019-11-01 11:24:17 +0100

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
 import unittest
-import args
 from main import app
 
 
@@ -13,10 +12,6 @@ class FlaskTestCase(unittest.TestCase):
         with app.test_client() as client:
             response = client.get('/video_feed', content_type='html/text')
             self.assertEqual(response.status_code, 200)
-
-    def test_parser(self):
-        parsed = args.parse_args(['--vid'])
-        self.assertEqual(parsed.vid, '25EgbhdVESE')
 
 
 if __name__ == '__main__':
